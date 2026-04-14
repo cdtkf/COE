8efe258ba3579fdc45d2bd6f357162dc39fe6a8b
Caleb Stoltz <cstoltz@reefpointgroup.com>
2026-04-14 12:51:17 -0400

    Add models __init__ with consolidated imports

---
+++ coe/models/__init__.py
@@ -0,0 +1,5 @@
+from coe.models.base import Base, TimestampMixin
+from coe.models.capability import ProposalServiceArea, ProposalCompetency, Proposal, ServiceArea, TechnicalCompetency, PastPerformance, DomainExperience
+from coe.models.staging import ProposedRecord
+from coe.models.scoring import ScoringRun, ScoringRetrievedRecord
+
